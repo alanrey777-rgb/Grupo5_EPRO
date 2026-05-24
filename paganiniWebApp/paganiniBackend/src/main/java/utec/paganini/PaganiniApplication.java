89b0d372621081f1e4c406fea3d0811cf6ec7a2a
MikaDev <camilo.ederson@gmail.com>
2026-05-23 22:31:46 -0600

    Agregadas carpetas bases de backend, frontend y base de datos

---
+++ paganiniWebApp/paganiniBackend/src/main/java/utec/paganini/PaganiniApplication.java
@@ -0,0 +1,13 @@
+package utec.paganini;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaganiniApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaganiniApplication.class, args);
+    }
+
+}
---
+++ paganiniWebApp/paganiniBackend/src/test/java/utec/paganini/PaganiniApplicationTests.java
@@ -0,0 +1,13 @@
+package utec.paganini;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaganiniApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
